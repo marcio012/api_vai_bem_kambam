a8955da3c1e943712a9ebf90a41f9ba5f96522e7
Márcio Heleno <marcio012@edu.unifor.br>
2020-09-02 03:09:48 -0300

    chore: criando os model mongoose

--- test/routes/usuario.test.ts
+++ test/routes/usuario.test.ts
@@ -1,10 +1,10 @@
 import request from 'supertest'
+import { UsuarioModel } from '../../src/database/schemas/usuarios'
 import app from '../../src/app'
 import Usuario from '../../src/models/usuario'
 
-const user: Usuario = {
-  id: 12,
-  nomeUsuario: 'marcio',
+const usuario: Usuario = {
+  nomeusuario: 'marcio',
   primeiroNome: 'Marcio',
   segundoNome: 'Pessoa',
   email: 'marcio@myemail.com',
@@ -12,52 +12,56 @@ const user: Usuario = {
 }
 
 describe('userRoute', () => {
+  beforeAll(() => {
+    return UsuarioModel.collection.drop().then(() => {
+      return UsuarioModel.collection.insert(usuario)
+    })
+  })
   it('deve responder com HTTP status 404 quando não há usuário', async () => {
-    const result = await request(app).get(`/usuarios/${user.nomeUsuario}`)
+    const result = await request(app).get(`/usuarios/${usuario.nomeusuario}`)
     expect(result.status).toEqual(404)
   })
   it('deve criar um usuário retorna-lo', async () => {
-    const result = await request(app).post(`/usuarios/`).send(user)
+    const result = await request(app).post(`/usuarios`).send(usuario)
     expect(result.status).toEqual(201)
-    expect(result.body.nomeUsuario).toEqual(user.nomeUsuario)
+    expect(result.body.nomeusuario).toEqual(usuario.nomeusuario)
   })
   it('deve retornar o usuário no banco', async () => {
-    const result = await request(app).get(`/usuarios/${user.nomeUsuario}`)
+    const result = await request(app).get(`/usuarios/${usuario.nomeusuario}`)
     expect(result.status).toEqual(200)
-    expect(result.body.nomeUsuario).toEqual(user.nomeUsuario)
+    expect(result.body.nomeusuario).toEqual(usuario.nomeusuario)
   })
   it('deve atualizar o usuário Marcio', async () => {
-    user.nomeUsuario = 'marcio Updated'
-    user.primeiroNome = 'Marcio Updated'
-    user.segundoNome = 'Pessoa Updated'
-    user.email = 'marcio@myemail_updated.com'
-    user.senha = '1234567Updated'
-    const result = await request(app).patch(`/usuarios/marcio`).send(user)
+    usuario.nomeusuario = 'marcio Updated'
+    usuario.primeiroNome = 'Marcio Updated'
+    usuario.segundoNome = 'Pessoa Updated'
+    usuario.email = 'marcio@myemail_updated.com'
+    usuario.senha = '1234567Updated'
+    const result = await request(app).patch(`/usuarios/marcio`).send(usuario)
     expect(result.status).toEqual(204)
   })
   it('deve retornar o usuário atualizado', async () => {
-    const result = await request(app).get(`/usuarios/${user.nomeUsuario}`)
+    const result = await request(app).get(`/usuarios/${usuario.nomeusuario}`)
 
     expect(result.status).toEqual(200)
-    expect(result.body.nomeUsuario).toEqual(user.nomeUsuario)
-    expect(result.body.primeiroNome).toEqual(user.primeiroNome)
-    expect(result.body.segundoNome).toEqual(user.segundoNome)
-    expect(result.body.email).toEqual(user.email)
-    expect(result.body.senha).toEqual(user.senha)
+    expect(result.body.nomeusuario).toEqual(usuario.nomeusuario)
+    expect(result.body.primeiroNome).toEqual(usuario.primeiroNome)
+    expect(result.body.segundoNome).toEqual(usuario.segundoNome)
+    expect(result.body.email).toEqual(usuario.email)
+    expect(result.body.senha).toEqual(usuario.senha)
   })
   it('deve remover um usuário existente', async () => {
-    const result = await request(app).del(`/usuarios/${user.nomeUsuario}`)
+    const result = await request(app).del(`/usuarios/${usuario}`)
     expect(result.status).toEqual(204)
   })
 
   it('deve retornar 404 se o usuário não existir', async () => {
-    user.nomeUsuario = 'bill'
-    const result = await request(app).patch(`/usuarios/bill`).send(user)
+    usuario.nomeusuario = 'bill'
+    const result = await request(app).patch(`/usuarios/bill`).send(usuario)
     expect(result.status).toEqual(404)
   })
   it('deve retornar 404 quando estiver tentando remover um usuário que não existe', async () => {
-    const result = await request(app).del(`/usuarios/Mary`)
-
+    const result = await request(app).del(`/usuarios/${usuario.nomeusuario}`)
     expect(result.status).toEqual(404)
   })
 })
